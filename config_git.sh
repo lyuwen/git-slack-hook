0afb68cc62073aee2df2681ee33df8e6768865fd
Lyuwen Fu <fulvwen@gmail.com>
2017-01-08 03:37:31 -0500

    Added support for webhook url to be save as a env var

--- config_git.sh
+++ config_git.sh
@@ -12,6 +12,12 @@ else
     ${ECHO} "Not found";
     exit 1;
 fi
+if [[ `uname|grep -ic darwin` -eq 1 ]];then
+    BASHRC=${HOME}/.bash_profile
+else
+    BASHRC=${HOME}/.bashrc
+fi
+cp $BASHRC ./`basename $BASHRC`.bak
 ${ECHO} -n "Check input... "
 REPO=$1
 while [[ -z $REPO ]]; do
@@ -28,27 +34,37 @@ ${ECHO} "All good, starting configuration..."
 ${ECHO} -n "Copying post-receive script..."
 if [[ -f "$REPO/hooks/post-receive" ]];then
     ${ECHO}
-    read -p "A post-receive script exists in the hooks directory, shall we overwrite(y/n)? " choice
+    read -p "A post-receive script exists in the hooks directory, shall we overwrite(Y/n)? " choice
     case "$choice" in 
         y|Y ) ${ECHO} "Proceeding";;
         n|N ) ${ECHO} "Exiting"; exit 1;;
-        * ) ${ECHO} "Exiting"; exit 1;;
+        * ) ${ECHO} "Proceeding";;
     esac
 fi
 cp -f git-slack-hook $REPO/hooks/post-receive
 ${ECHO} "done"
 ${ECHO} "Congiuring the repository, entering the repository."
 cd $REPO
-${ECHO}
-${ECHO} "Now please go to the following website to retrieve a webhook URL:"
-${ECHO} "    https://my.slack.com/services/new/incoming-webhook"
-${ECHO}
-read -p "Please type in the webhook URL: " WHURL
-${ECHO} -n "checking... "
-if [[ ! `${ECHO} "$WHURL"|grep -c "https://hooks.slack.com/services"` -eq 1 ]];then
+if [[ ! -z "${SLACK_WEBHOOK_URL}" ]] && [[ `${ECHO} "${SLACK_WEBHOOK_URL}"|grep -c "https://hooks.slack.com/services"` -eq 1 ]]; then
+    ${ECHO} "Found env variable SLACK_WEBHOOK_URL, proceeding with this."
+    WHURL=${SLACK_WEBHOOK_URL}
+else
     ${ECHO}
-    ${ECHO} "invalid URL"
-    exit 1;
+    ${ECHO} "Now please go to the following website to retrieve a webhook URL:"
+    ${ECHO} "    https://my.slack.com/services/new/incoming-webhook"
+    ${ECHO}
+    read -p "Please type in the webhook URL: " WHURL
+    ${ECHO} -n "checking... "
+    if [[ ! `${ECHO} "$WHURL"|grep -c "https://hooks.slack.com/services"` -eq 1 ]];then
+        ${ECHO}
+        ${ECHO} "invalid URL"
+        exit 1;
+    fi
+    read -p "Save the URL as an env var(Y/n)? " choice
+    case "$choice" in 
+        n|N ) ${ECHO} "Skipping"; exit 1;;
+        * ) ${ECHO} "${ECHO} \"export SLACK_WEBHOOK_URL=\"\$\{WHURL\}\"\"";${ECHO} "export SLACK_WEBHOOK_URL=\"${WHURL}\"" >> $BASHRC; exit 1;;
+    esac
 fi
 #${ECHO} "git config hooks.slack.webhook-url \"$WHURL\""
 git config hooks.slack.webhook-url "$WHURL"
@@ -73,7 +89,7 @@ read -p "Please specicy a repository nice name (optional): " nicename
 if [[ ! -z $nicename ]]; then
     git config hooks.slack.repo-nice-name "$nicename"
 fi
-read -p "Display full/last commit(s) when pushing mutiple commits (y/n)? " choice
+read -p "Display full/last commit(s) when pushing mutiple commits (y/N)? " choice
 case "$choice" in 
     y|Y ) ${ECHO} "Full commits mode.";git config hooks.slack.show-full-commit true;;
     * ) ${ECHO} "Last commit mode."; git config hooks.slack.show-only-last-commit true;;
